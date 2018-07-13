95bb4f80c083f70c25a39634a1faa69071ac0f1a
Nareshkeerapatla <k8801409938@gmail.com>
2018-07-13 10:54:18 +0530

    Initial commit

---
+++ HerokuApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+	return HttpResponse("<h1>Hello world! it's  my first live website</h1>")
